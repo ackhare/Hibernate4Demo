401298bbc24c6df565285a1f9b58f48d8164addc
chetan <bairagi.sirfire@gmail.com>
2018-03-25 15:33:09 +0530

    Inheritance mapping

---
+++ src/main/java/com/chetan/InheritanceMapping/OneTablePeConcreteClass/HibernateUtil.java
@@ -0,0 +1,36 @@
+package com.chetan.InheritanceMapping.OneTablePeConcreteClass;
+ 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+ 
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+ 
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            Configuration configuration = new Configuration();
+            configuration.configure("com/chetan/InheritanceMapping/OneTablePeConcreteClass/hibernate.cfg.xml");
+            System.out.println("Hibernate Annotation Configuration loaded");
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            System.out.println("Hibernate Annotation serviceRegistry created");
+
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            return sessionFactory;
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+ 
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+
---
+++ src/main/java/com/chetan/InheritanceMapping/OneTablePerHeirarchy/HibernateUtil.java
@@ -0,0 +1,35 @@
+package com.chetan.InheritanceMapping.OneTablePerHeirarchy;
+ 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+ 
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+ 
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            Configuration configuration = new Configuration();
+            configuration.configure("com/chetan/InheritanceMapping/OneTablePerHeirarchy/hibernate.cfg.xml");
+            System.out.println("Hibernate Annotation Configuration loaded");
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            System.out.println("Hibernate Annotation serviceRegistry created");
+
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            return sessionFactory;
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+ 
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+
